88468b3decda237e46af2a5358e1b5e53f245cc9
YumeLoL <yumeee.lol@gmail.com>
2024-09-19 19:44:35 +1000

    project pagination & sorting

--- resources/js/Pages/Project/Index.jsx
+++ resources/js/Pages/Project/Index.jsx
@@ -17,6 +17,7 @@ export default function Index({ auth, projects, queryParams = null, success }) {
     } else {
       delete queryParams[name];
     }
+    console.log("check query", queryParams);
 
     router.get(route("project.index"), queryParams);
   };
